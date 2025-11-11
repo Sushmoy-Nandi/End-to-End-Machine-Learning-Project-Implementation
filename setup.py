baddd14b8036064f7c026cc023a69518687b4b9d
“Sushmoy <learnsushmoysk1091@gmail.com>
2025-11-11 20:38:34 +0600

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Sushmoy Nandi",
+    author_email="learnsushmoysk1091@gmail.com",
+    packages=find_packages()
+)
