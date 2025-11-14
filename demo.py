1f3345e3dfeec35e0b1b07e387aa09fa4881f250
“Sushmoy <learnsushmoysk1091@gmail.com>
2025-11-14 17:56:54 +0600

    data ingesation component added

--- demo.py
+++ demo.py
@@ -1,13 +1,8 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-import sys
+from us_visa.pipline.training_pipeline import TrainingPipeline
+
+pipeline = TrainingPipeline()
+pipeline.run_pipeline()
 
 
-try:
-    r = 3/0
-    print(r)
-except Exception as e:
-    logging.info(e)
-    raise USvisaException(e, sys)
 
 
